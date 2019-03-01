03b36d08b455d2b890a22a071fafa1621e9dfe60
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2019-03-01 14:20:52 -0300

    Aproveitando token

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -30,6 +30,17 @@ export class HomePage {
   ionViewDidLeave(){
     this.menu.swipeEnable(true);
   }
+
+  ionViewDidEnter(){
+    this.auth.refreshToken()
+      .subscribe(res => {
+        this.auth.successfulLogin(res.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {})
+  }
+  
+
   login(){
     this.auth.authenticate(this.creds)
       .subscribe(res => {
