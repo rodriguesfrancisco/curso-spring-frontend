c6d9ef106db433149480d6dc32b29d9cac0cf4a9
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2019-03-07 14:13:32 -0300

    Salvando usuário

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -29,6 +30,9 @@ export class ErrorInterceptor implements HttpInterceptor {
                     break;
                     case 403:
                         this.handle403();
+                    break;
+                    case 422:
+                        this.handle422(errorObj);
                     break;
                     default:
                         this.handleDefaultError(errorObj);
@@ -67,6 +71,27 @@ export class ErrorInterceptor implements HttpInterceptor {
         });
 
         alert.present();
+    }
+
+    handle422(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'Ok'}
+            ]
+        });
+
+        alert.present();
+    }
+
+    listErrors(messages: FieldMessage[]) : string{
+        let s : string = '';
+        for(var i = 0; i < messages.length; i++){
+            s = s + "<p><strong>" + messages[i].fieldName + "</strong>: " + messages[i].message + "</p>";
+        }
+        return s;
     }
 }
 
